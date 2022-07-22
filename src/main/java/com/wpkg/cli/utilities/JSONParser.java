20a889209b343ed04b25b2008fbd8e006da5b408
typeable-nickname <74413191+typeable-nickname@users.noreply.github.com>
2022-07-23 00:28:21 +0200

    fix: using json

--- src/main/java/com/wpkg/cli/utilities/JSONParser.java
+++ src/main/java/com/wpkg/cli/utilities/JSONParser.java
@@ -11,19 +11,7 @@ public class JSONParser {
 
         ClientJSON clientJSON;
         try {
-            clientJSON = objectMapper.readValue("""
-                    {
-                        "clients":[
-                            {
-                                "name":"RAT: 0",
-                                "id":0
-                            },
-                            {
-                                "name":"RAT: 1",
-                                "id":1
-                            }
-                        ]
-                    }""", ClientJSON.class);
+            clientJSON = objectMapper.readValue(json, ClientJSON.class);
 
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
@@ -33,7 +21,8 @@ public class JSONParser {
     public static void setList(DefaultListModel<String> listModel, String json){
         ClientJSON list = getClientList(json);
         for(int i = 0; i < list.clients.length; i++){
-            listModel.addElement("\uD83D\uDDA5️ ID: "+list.clients[i].id+" NAME: "+list.clients[i].name);
+            listModel.addElement("\uD83D\uDDA5️     \uD83D\uDCB3 ID: "+list.clients[i].id + "        "
+                                                +" \uD83D\uDCD6 NAME: "+list.clients[i].name);
         }
     }
 }
