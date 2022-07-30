a2780be4072d675e9f123b32fe548e141d829900
rafi612 <rafi612lol@gmail.com>
2022-07-30 16:00:42 +0200

    feat: added screenshots via imgur

--- src/main/java/com/wpkg/cli/commands/Screenshot.java
+++ src/main/java/com/wpkg/cli/commands/Screenshot.java
@@ -6,6 +6,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.util.Date;
 
@@ -15,7 +18,9 @@ public class Screenshot extends Command{
 
     static {
         try {
-            tmpdir = Files.createTempDirectory("tmpDirPrefix").toFile().getAbsolutePath();
+            File tmp = Files.createTempDirectory("wpkgcli").toFile();
+            tmp.deleteOnExit();
+            tmpdir = tmp.getAbsolutePath();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -28,42 +33,19 @@ public class Screenshot extends Command{
     @Override
     public void execute()
     {
-        //sending command and receiving buffer size
+        String url = sendCommand("screenshot");
 
-        sendToServer("screenshot");
-
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-        int buffersize = Integer.parseInt(receiveFromServer());
-
-        //sending receive to server
-        sendToServer("received");
-
-        String screenshotpath = tmpdir + File.separator + "screenshot" + new Date() + ".png";
-        try (FileOutputStream fos = new FileOutputStream(screenshotpath))
+        if (Desktop.isDesktopSupported())
         {
-            int index = 0;
-            while (index < buffersize)
-            {
-                byte[] buff = receiveRawdata();
-                fos.write(buff);
-                index += buff.length;
+            try {
+                Desktop.getDesktop().browse(new URI(url));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (URISyntaxException e) {
+                throw new RuntimeException(e);
             }
-
-            if (Desktop.isDesktopSupported())
-            {
-                Desktop.getDesktop().open(new File(screenshotpath));
-            }
-
         }
-        catch (IOException e)
-        {
-            throw new RuntimeException(e);
-        }
+
 
     }
 }
