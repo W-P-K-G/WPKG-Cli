b473dafeb26c21841a3c989ada5613d7d1f550f3
rafi612 <rafi612lol@gmail.com>
2022-11-19 19:46:13 +0100

    fix: url

--- src/main/java/me/wpkg/cli/utils/Globals.java
+++ src/main/java/me/wpkg/cli/utils/Globals.java
@@ -5,5 +5,5 @@ import java.io.File;
 public class Globals {
     public static File workDir = new File(Tools.getWorkDir());
     public static File passwordFile;
-    public static String jsonURL = "https://wpkg.me/WPKG/JSONFiles/";
+    public static String jsonURL = "http://opgl.ct8.pl/WPKG/JSONFiles/";
 }
